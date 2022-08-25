584bcd6c20c83e0d864edd5bce893d9aa24cf737
Håkon Tansem <haakont@pc5533.pc.met.no>
2022-08-25 14:28:00 +0200

    Made a more sensible test

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,41 @@
+import datetime
+import pytz
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
 from http import HTTPStatus
-import datetime
+from dateutil import parser
 
 client = TestClient(app)
 
 def test_known_date():
     """
-    This simple request should never fail
+    Sunrise in Oslo 2010-12-24 occured on 09:19 local time
+    Sunset in Oslo 2010-12-24 occured on 15:13 local time
+    source (https://www.timeanddate.com/sun/norway/oslo?month=12&year=2010)
+
+    Check that the Sunrise application reproduses this plus minus a few minutes
     """
-    response = client.get("/.json?date=2022-08-22&lat=51.477&lon=-0.001&elevation=0")
+    response = client.get("/.json?date=2010-12-24&lat=59.54&lon=10.45&elevation=0")
+    response_json = response.json()
+    sunset = response_json["time"][0]["sunset"]["time"]
+    sunrise = response_json["time"][0]["sunrise"]["time"]
+    sunset = parser.parse(sunset)
+    sunrise = parser.parse(sunrise)
+    timezone = pytz.timezone("Europe/Oslo")
+    min_tol_sunset = datetime.datetime(2010, 12, 24, 15, 11, 0)
+    max_tol_sunset = datetime.datetime(2010, 12, 24, 15, 17, 0)
+    min_tol_sunrise = datetime.datetime(2010, 12, 24, 9, 16, 0)
+    max_tol_sunrise = datetime.datetime(2010, 12, 24, 9, 23, 0) 
     assert response.status_code == HTTPStatus.OK
-
-
-
+    assert timezone.localize(min_tol_sunset) < sunset < timezone.localize(max_tol_sunset)
+    assert timezone.localize(min_tol_sunrise) < sunrise < timezone.localize(max_tol_sunrise)
+    
 def test_north_pole():
     """
     Only one sunset and sunrise per year on the north pole.
     """
+    
     start_date = datetime.date(2022, 1, 1)
     end_date = datetime.date(2022, 12, 31)
     delta = datetime.timedelta(days=1)
